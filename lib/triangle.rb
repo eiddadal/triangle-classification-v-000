e9f3a1363ab92b0d9cdb700795c415af0f0d4696
Dalya Eidda <dalia.eidda@gmail.com>
2019-05-09 23:52:25 +0000

    Automatically backed up by Learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -1,3 +1,21 @@
 class Triangle
-  # write code here
+  attr_accessor :length_1, :length_2, :length_3
+  
+  def initialize(length_1, length_2, length_3)
+    @length_1 = length_1
+    @length_2 = length_2
+    @length_3 = length_3
+  end 
+  
+  def kind 
+    :equilateral
+
+:isosceles
+
+:scalene
+  end 
+  
+  class TriangleError < StandardError 
+  end
+  
 end
