95befe70e8aff434b5094edf1995e4c10a1616c3
Dalya Eidda <dalia.eidda@gmail.com>
2019-05-10 00:35:10 +0000

    Automatically backed up by Learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -8,6 +8,10 @@ class Triangle
   end 
   
   def kind
+   if a == b || b == c || a == c 
+     return :isosceles
+     e
+   end 
    raise TriangleError if a<=0 or b<=0 or c<=0
   raise TriangleError if a+b<=c or b+c<=a or a+c<=b
   return :equilateral if a==b and a==c                 
