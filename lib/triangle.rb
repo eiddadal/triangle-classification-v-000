9306d35de66ea1f444ee4802fc4f295d61864a59
Dalya Eidda <dalia.eidda@gmail.com>
2019-05-10 00:32:31 +0000

    Automatically backed up by Learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -8,11 +8,11 @@ class Triangle
   end 
   
   def kind(a, b, c)
-    a, b, c = [a, b, c].sort
-  raise TriangleError if a <= 0 or a + b <= c
-  return :equilateral if a == c
-  return :isosceles if a == b or b == c
-  return :scalene
+   raise TriangleError if a<=0 or b<=0 or c<=0
+  raise TriangleError if a+b<=c or b+c<=a or a+c<=b
+  return :equilateral if a==b and a==c                 
+  return :isosceles if a==b or b==c or a==c
+  :scalene 
   end 
   
   
