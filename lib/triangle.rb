5692ee99eb2d1b85ba1c0d1e946775f27ea21892
Dalya Eidda <dalia.eidda@gmail.com>
2019-05-10 00:37:10 +0000

    Automatically backed up by Learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -18,12 +18,7 @@ class Triangle
    return : equilateral
  else 
    :scalene
-   end 
-   raise TriangleError if a<=0 or b<=0 or c<=0
-  raise TriangleError if a+b<=c or b+c<=a or a+c<=b
-  return :equilateral if a==b and a==c                 
-  return :isosceles if a==b or b==c or a==c
-  :scalene 
+   
   end 
   
   
