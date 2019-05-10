4137508d148e549684e62864a2d3d05236f9ceac
Dalya Eidda <dalia.eidda@gmail.com>
2019-05-10 00:23:36 +0000

    Automatically backed up by Learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -8,7 +8,7 @@ class Triangle
   end 
   
   def kind(length_1, length_2, length_3)
-    if length_1 == length_2 || length_2 == length_3 || length_3 == length_1
+    if length_1 = length_2 || length_2 = length_3 || length_3 = length_1
       equilateral
 
 isosceles
